c3c2d229a7051f1c8d588b7f5572fdbe2ea8484f
Hedgehog-blue <d.spirina@ukma.edu.ua>
2025-06-10 21:05:10 +0300

    structure

---
+++ task.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value):
+        self.value = value 
+        self.next = None 
+
+         
+class LinkedList:
+    def __init__(self):
+        pass
+
+
+    def add(self, value):
+        pass
+
+
+    def get_length(self):
+        pass
+
+
+    def get_element(self):
+        pass
+
+    
+
+
